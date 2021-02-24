2787eaa0adc458d07f55de5b65ebfb25bc7dbdb4
GaBorin <gabriel_borin.lock@hotmail.com>
2021-02-24 00:08:36 -0300

    conexao com o banco

--- connectionDB.php
+++ connectionDB.php
@@ -1,6 +1,6 @@
 <?php
 	// Create connection
-	$link = new MySQLi('us-cdbr-east-03.cleardb.com', 'bab2e37d9ae451', '695f7765');
+	$link = new MySQLi('localhost', 'borin.eu5.org', '7LFv9hbgWz_FYjh', '242517');
 
 	// Check connection
 	if (!$link) {
